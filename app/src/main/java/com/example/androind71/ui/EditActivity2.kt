8e7f99c89c96238453409f3d0086c86db1c78b5a
Arik1021 <aaa100107aaa@gmail.com>
2023-02-23 19:02:18 +0600

    second commit

--- app/src/main/java/com/example/androind71/ui/EditActivity2.kt
+++ app/src/main/java/com/example/androind71/ui/EditActivity2.kt
@@ -2,28 +2,28 @@ package com.example.androind71.ui
 
 import android.app.Activity
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import com.example.androind71.Key
-import com.example.androind71.databinding.EditActivityBinding
+import com.example.androind71.databinding.ActivityEdit2Binding
 
-class EditActivity : AppCompatActivity() {
-    private lateinit var binding: EditActivityBinding
+class EditActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityEdit2Binding
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState)
-        binding = EditActivityBinding.inflate(LayoutInflater.from(parent))
-
-        setContentView(binding.root)
+        binding = ActivityEdit2Binding.inflate(LayoutInflater.from(parent))
         backLauncher()
         backListeners()
         getData()
+        setContentView(binding.root)
+        
     }
 
     private fun backLauncher() {
