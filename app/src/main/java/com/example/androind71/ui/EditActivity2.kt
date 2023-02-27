c0948293710a5b623db0d13b3b08ae398b3efcf0
Arik1021 <aaa100107aaa@gmail.com>
2023-02-27 18:56:46 +0600

    third commit

--- app/src/main/java/com/example/androind71/ui/EditActivity2.kt
+++ app/src/main/java/com/example/androind71/ui/EditActivity2.kt
@@ -1,34 +1,41 @@
 package com.example.androind71.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.LayoutInflater
 import android.widget.Toast
-import com.example.androind71.Key
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.androind71.R
 import com.example.androind71.databinding.ActivityEdit2Binding
 
 class EditActivity2 : AppCompatActivity() {
     private lateinit var binding: ActivityEdit2Binding
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityEdit2Binding.inflate(LayoutInflater.from(parent))
-        backListeners()
+        binding = ActivityEdit2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        backListeners()
 
     }
 
     private fun backListeners() {
+        binding.textBackTv.setText(intent.getStringExtra(KEY_FOR_DATA))
         binding.backBtn.setOnClickListener {
             if (binding.textBackTv.text.isNotEmpty()) {
                 val text = binding.textBackTv.text.toString()
-                setResult(RESULT_OK, Intent().putExtra(Key.KEY_FOR_DATA, text))
+                setResult(RESULT_OK, Intent().putExtra(KEY_FOR_DATA, text))
                 finish()
             } else {
-                Toast.makeText(this, "You should type something", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, getString(R.string.toastForActivity), Toast.LENGTH_SHORT)
+                    .show()
             }
         }
+    }
+
+    companion object {
+        const val KEY_FOR_DATA = "data"
     }
 
 
--- app/src/main/java/com/example/androind71/ui/MainActivity.kt
+++ app/src/main/java/com/example/androind71/ui/MainActivity.kt
@@ -5,45 +5,45 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.androind71.Key
 import com.example.androind71.databinding.ActivityMainBinding
+import com.example.androind71.ui.EditActivity2.Companion.KEY_FOR_DATA
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-    private val resultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                binding.textEt.setText(result.data?.getStringExtra(Key.KEY_FOR_DATA))
-            }
-        }
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        initListeners()
         setContentView(binding.root)
-
+        initListeners()
 
     }
 
     private fun initListeners() {
 
         binding.intentBtn.setOnClickListener {
-
             if (binding.textEt.text.isNotEmpty()) {
-                resultLauncher.launch(
-                    Intent(
-                        this,
-                        EditActivity2::class.java
-                    ).apply { putExtra(Key.KEY_FOR_DATA, binding.textEt.text.toString()) })
-                } else {
+                val intent = Intent(this, EditActivity2::class.java)
+                intent.putExtra(KEY_FOR_DATA, binding.textEt.text.toString())
+                resultLauncher.launch(intent)
+            } else {
                 Toast.makeText(this, "You should type something", Toast.LENGTH_SHORT).show()
             }
 
         }
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                binding.textEt.setText(it.data?.getStringExtra(KEY_FOR_DATA))
+            }
+    }
 
+
+    companion object {
+        const val KEY_FOR_DATA = "data"
     }
 
 
